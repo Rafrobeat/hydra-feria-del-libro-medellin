9b96e4237f3d47f6ab37ea2607cbb8cc1f7f682d
rafrobeat <rafaeldiazc@me.com>
2025-09-04 10:47:28 -0500

    first release

---
+++ public/rafapuyana/js/rafapuyana.js
@@ -0,0 +1,7 @@
+// rafapuyana.js
+console.log("rafapuyana-js loaded");
+
+// Frases personalizadas
+querido = () => osc().out();
+lucilio = () => osc().kaleid().out();
+queridoLucilio = () => osc(1,1,1).kaleid().out();
