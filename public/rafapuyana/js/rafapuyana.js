1634a8909ca5c31a1ccee9e05373a627b22dbab7
rafrobeat <rafaeldiazc@me.com>
2025-09-05 07:15:42 -0500

    git fixes font + new lines

--- public/rafapuyana/js/rafapuyana.js
+++ public/rafapuyana/js/rafapuyana.js
@@ -5,3 +5,11 @@ console.log("rafapuyana-js loaded");
 querido = () => osc().out();
 lucilio = () => osc().kaleid().out();
 queridoLucilio = () => osc(1,1,1).kaleid().out();
+
+// Frases personalizadas;
+test = () => s1.initImage("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1920px-Image_created_with_a_mobile_phone.png")
+test2 = () => src(s1).out(o0);
+
+test3 = () => s1.initImage("./public/rafapuyana/images/body.png")
+test4 = () => src(s1).out(o0);
+//shape().out(o0)
