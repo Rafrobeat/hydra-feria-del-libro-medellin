ddce6cd95eb8113e19886240deb889213886cb3e
rafrobeat <rafaeldiazc@me.com>
2025-09-06 10:58:50 -0500

    fixed time load hydra

--- public/rafapuyana/js/rafapuyana.js
+++ public/rafapuyana/js/rafapuyana.js
@@ -2,28 +2,28 @@
 console.log("rafapuyana-js loaded");
 
 // Cargar Hydra para poder cargar lasd imagenes
-// window.addEventListener('DOMContentLoaded', function() {
-//     // Inicializar Hydra
-//     const hydra = new Hydra({
-//         canvas: document.getElementById('hydra-canvas'),
-//         detectAudio: false,
-//         enableStreamCapture: false,
-//         makeGlobal: true 
-//     });
+window.addEventListener('DOMContentLoaded', function() {
+    // Inicializar Hydra
+    const hydra = new Hydra({
+        canvas: document.getElementById('hydra-canvas'),
+        // detectAudio: false,
+        // enableStreamCapture: false,
+        makeGlobal: true 
+    });
 
-//     // Esperar a que Hydra esté completamente inicializado
-//     setTimeout(() => {
-//         try {
-//             // Precargar imagenes
-//             s0.initImage("./public/rafapuyana/images/01.jpg");
-//             s1.initImage("./public/rafapuyana/images/02.jpg");
-//             s2.initImage("./public/rafapuyana/images/03.jpg");
-//             s3.initImage("./public/rafapuyana/images/04.jpg");
-//         } catch (error) {
-//             console.error("Error al cargar las imagenes:", error);
-//         }
-//     }, 100);
-// });
+    // Esperar a que Hydra esté completamente inicializado
+    setTimeout(() => {
+        try {
+            // Precargar imagenes
+            s0.initImage("./public/rafapuyana/images/01.jpg");
+            s1.initImage("./public/rafapuyana/images/02.jpg");
+            s2.initImage("./public/rafapuyana/images/03.jpg");
+            s3.initImage("./public/rafapuyana/images/04.jpg");
+        } catch (error) {
+            console.error("Error al cargar las imagenes:", error);
+        }
+    }, 500);
+});
 
 // Frases personalizadas
 querido = () => osc().out();
@@ -40,14 +40,14 @@ test4 = () => src(s1).out(o0);
 //shape().out(o0)
 
 // precarga otras imagenes
-// function precargarImagenes() {
-//     try {
-//             // Precargar imagenes
-//             s0.initImage("./public/rafapuyana/images/05.jpg");
-//             s1.initImage("./public/rafapuyana/images/06.jpg");
-//             s2.initImage("./public/rafapuyana/images/07.jpg");
-//             s3.initImage("./public/rafapuyana/images/08.jpg");
-//     } catch (error) {
-//         console.error("Error al cargar paquete de imagenes:", error);
-//     }
-// }
+function precargarImagenes() {
+    try {
+            // Precargar imagenes
+            s0.initImage("./public/rafapuyana/images/05.jpg");
+            s1.initImage("./public/rafapuyana/images/06.jpg");
+            s2.initImage("./public/rafapuyana/images/07.jpg");
+            s3.initImage("./public/rafapuyana/images/08.jpg");
+    } catch (error) {
+        console.error("Error al cargar paquete de imagenes:", error);
+    }
+}
