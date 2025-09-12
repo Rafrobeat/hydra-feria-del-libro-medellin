1e696a4933056a6e0881a7e16eba2d18e6c21d16
Rafa <rfpuyana@gmail.com>
2025-09-11 22:13:29 -0500

    version estable ready for performance Jueves 11 pm

--- public/rafapuyana/js/contradicciones.js
+++ public/rafapuyana/js/contradicciones.js
@@ -1,9 +1,10 @@
 Habitar_las = (cam=1) => s1.initCam(cam);
+
 Contradicciones = () =>
   src(s1)
   	.modulateHue(osc(.01,.01,.01),1000)
   	.modulateScale(noise(.1,.1),.1)
-  	.blend(solid(0, 0.278, 0.031),0)
+  	.blend(solid(0, 0.278, 0.031),0.3)
   	.scale(1,[1.001,1].smooth(.2),.99,3,.7)
   	.blend(o0,.6)
   	//.modulate(o0,.0001)
@@ -23,7 +24,8 @@ Cándido_ = () =>
     src(s1)
     	.modulateHue(osc(.01,.01,.01),1000)
     	.modulateScale(noise(.1,.1),.1)
-    	.blend(solid(0, 0.278, 0.031),0)
+      .modulateScale((noise(.1,.1)),.2)
+    	.blend(solid(0, 0.278, 0.031),0.3)
     	.scale(1,[1.001,1].smooth(.2),.99,3,.7)
     	.blend(o0,.6)
     	//.modulate(o0,.0001)
